e09b32a28695d6ce574e939c61dd193165cb3f92
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-01-03 22:44:10 +0530

    Basic routing done

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
---
+++ src/routes/invoices.jsx
@@ -0,0 +1,7 @@
+export default function Invoices() {
+  return (
+    <main stryle={{ padding: "1rem 0" }}>
+      <h2>Invoices</h2>
+    </main>
+  );
+}
